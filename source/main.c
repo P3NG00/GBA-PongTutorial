cabbb1eff9426aa23116fa164211e7e7e7611f20
P3NG00 <p3ng00minecraft@gmail.com>
2025-01-17 14:09:37 -0700

    implement player and ball movement with previous position clearing

--- source/main.c
+++ source/main.c
@@ -14,8 +14,12 @@ typedef u16             M3LINE[SCREEN_WIDTH];
 struct rect {
     int x;
     int y;
+    int prevX;
+    int prevY;
     int width;
     int height;
+    int velocityX;
+    int velocityY;
 };
 
 void drawPixel(int x, int y, int color) {
@@ -30,9 +34,15 @@ void drawRect(struct rect* cRect) {
         }
     }
 }
+void clearPrevious(struct rect* cRect) {
+    for (int x = cRect->prevX; x < cRect->prevX + cRect->width; x++) {
+        for (int y = cRect->prevY; y < cRect->prevY + cRect-> height; y++) {
+            drawPixel(x, y, 0x0000);
+        }
+    }
+}
 
 int main(void) {
-
     irqInit();
     irqEnable(IRQ_VBLANK);
 
@@ -41,19 +51,82 @@ int main(void) {
 
     struct rect humanPlayer;
     humanPlayer.x = 1;
-    humanPlayer.y = 60;
+    humanPlayer.y = SCREEN_HEIGHT / 2 - 24 / 2;
+    humanPlayer.prevX = humanPlayer.x;
+    humanPlayer.prevY = humanPlayer.y;
     humanPlayer.width = 8;
     humanPlayer.height = 24;
+    humanPlayer.velocityX = 0;
+    humanPlayer.velocityY = 0;
+
+    struct rect computerPlayer;
+    computerPlayer.x = SCREEN_WIDTH - 8 - 1;
+    computerPlayer.y = SCREEN_HEIGHT / 2 - 24 / 2;
+    computerPlayer.prevX = computerPlayer.x;
+    computerPlayer.prevY = computerPlayer.y;
+    computerPlayer.width = 8;
+    computerPlayer.height = 24;
+    computerPlayer.velocityX = 0;
+    computerPlayer.velocityY = 0;
+
+    struct rect ball;
+    ball.x = SCREEN_WIDTH / 2 - 8 / 2;
+    ball.y = SCREEN_HEIGHT / 2 - 8 / 2;
+    ball.prevX = ball.x;
+    ball.prevY = ball.y;
+    ball.width = 8;
+    ball.height = 8;
+    ball.velocityX = 0;
+    ball.velocityY = 0;
 
     /* Draw Center Line */
     for (int i = 0; i < SCREEN_HEIGHT; i++) {
         drawPixel(SCREEN_WIDTH / 2, i, 0x7FFF);  // White pixels
     }
 
-    /* Draw Player */
-    drawRect(&humanPlayer);
-
     while (1) {
         VBlankIntrWait();
+
+        /* Respond to input */
+        scanKeys();
+        int key_pressed = keysDown();
+        int key_released = keysUp();
+
+        /* If up or down released -> velocity = 0 */
+        if ((key_released & KEY_UP) || (key_released & KEY_DOWN)) {
+            humanPlayer.velocityY = 0;
+        }
+        /* If up is pressed -> velocity = -2 */
+        if ((key_pressed & KEY_UP) && humanPlayer.y >= 0) {
+            // Move up
+            humanPlayer.velocityY = -2;
+        }
+        /* If down is pressed -> velocity = 2 */
+        if ((key_pressed & KEY_DOWN) && humanPlayer.y <= SCREEN_HEIGHT -
+            humanPlayer.height) {
+                // Move down
+                humanPlayer.velocityY = 2;
+        }
+        /* If pressed but at ceiling or floor, don't continue */
+        if ((humanPlayer.y <= 0 && humanPlayer.velocityY < 0) ||
+            ((humanPlayer.y >= SCREEN_HEIGHT - humanPlayer.height) &&
+            humanPlayer.velocityY > 0)) {
+                humanPlayer.velocityY = 0;
+        }
+
+        humanPlayer.y += humanPlayer.velocityY;
+
+        /* Clear Previous */
+        clearPrevious(&ball);
+        clearPrevious(&humanPlayer);
+        clearPrevious(&computerPlayer);
+
+        /* Draw Players */
+        drawRect(&ball);
+        drawRect(&humanPlayer);
+        drawRect(&computerPlayer);
+
+        humanPlayer.prevX = humanPlayer.x;
+        humanPlayer.prevY = humanPlayer.y;
     }
 }
