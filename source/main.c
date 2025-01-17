2781ba1125850f0bb5fa7d95693404ffbca79772
P3NG00 <p3ng00minecraft@gmail.com>
2025-01-17 10:01:22 -0700

    center line

--- source/main.c
+++ source/main.c
@@ -1,34 +1,33 @@
-
-#include <gba_console.h>
 #include <gba_video.h>
 #include <gba_interrupt.h>
 #include <gba_systemcalls.h>
 #include <gba_input.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-//---------------------------------------------------------------------------------
-// Program entry point
-//---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
+#define MEM_VRAM        0x06000000
+#define SCREEN_WIDTH    240
+#define SCREEN_HEIGHT   160
 
+typedef u16             M3LINE[SCREEN_WIDTH];
+#define m3_mem          ((M3LINE*)MEM_VRAM)
 
-	// the vblank interrupt must be enabled for VBlankIntrWait() to work
-	// since the default dispatcher handles the bios flags no vblank handler
-	// is required
-	irqInit();
-	irqEnable(IRQ_VBLANK);
-
-	consoleDemoInit();
-
-	// ansi escape sequence to set print co-ordinates
-	// /x1b[line;columnH
-	iprintf("\x1b[10;10HPONG TUTORIAL BITCHES!!!\n");
-
-	while (1) {
-		VBlankIntrWait();
-	}
+void drawPixel(int x, int y, int color) {
+    m3_mem[y][x] = color;
 }
 
+int main(void) {
 
+    irqInit();
+    irqEnable(IRQ_VBLANK);
+
+    /* Set GBA to Mode 3 (Bitmap Mode) */
+    SetMode( MODE_3 | BG2_ON );
+
+    /* Draw Center Line */
+    for (int i = 0; i < SCREEN_HEIGHT; i++) {
+        drawPixel(SCREEN_WIDTH / 2, i, 0x7FFF);  // White pixels
+    }
+
+    while (1) {
+        VBlankIntrWait();
+    }
+}
