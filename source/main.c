e37bae7e6fe9478e5b3c6d72c1b58ae97aa1287b
P3NG00 <p3ng00minecraft@gmail.com>
2025-01-17 10:07:40 -0700

    add rectangle structure and drawing function for player representation

--- source/main.c
+++ source/main.c
@@ -10,8 +10,25 @@
 typedef u16             M3LINE[SCREEN_WIDTH];
 #define m3_mem          ((M3LINE*)MEM_VRAM)
 
+/* Players and Ball Rectangles */
+struct rect {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
 void drawPixel(int x, int y, int color) {
     m3_mem[y][x] = color;
+}
+
+/* Draw Player / Ball */
+void drawRect(struct rect* cRect) {
+    for (int x = cRect->x; x < cRect->x + cRect->width; x++) {
+        for (int y = cRect->y; y < cRect->y + cRect-> height; y++) {
+            drawPixel(x, y, 0x7FFF);
+        }
+    }
 }
 
 int main(void) {
@@ -22,10 +39,19 @@ int main(void) {
     /* Set GBA to Mode 3 (Bitmap Mode) */
     SetMode( MODE_3 | BG2_ON );
 
+    struct rect humanPlayer;
+    humanPlayer.x = 1;
+    humanPlayer.y = 60;
+    humanPlayer.width = 8;
+    humanPlayer.height = 24;
+
     /* Draw Center Line */
     for (int i = 0; i < SCREEN_HEIGHT; i++) {
         drawPixel(SCREEN_WIDTH / 2, i, 0x7FFF);  // White pixels
     }
+
+    /* Draw Player */
+    drawRect(&humanPlayer);
 
     while (1) {
         VBlankIntrWait();
